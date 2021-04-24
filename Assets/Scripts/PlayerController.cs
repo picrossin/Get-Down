e48f052470a724c5eba59c79e3a414afb60d03b5
picrossin <jadengoter@gmail.com>
2021-04-24 15:38:10 -0500

    Worked on vinyl and cube

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-using System.Runtime.CompilerServices;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
     [Header("Settings")] 
     [SerializeField] [Range(0f, 50f)] private float walkSpeed;
     [SerializeField] [Range(0f, 100f)] private float throwSpeed;
-
+    [SerializeField] private int totalRecords = 1;
+    [SerializeField] private int catchFramesTotal = 5;
+    [SerializeField] private float recordCoyoteSeconds = 0.1f;
+    
     [Header("Dependencies")] 
     [SerializeField] private GameObject sprite;
     [SerializeField] private GameObject record;
@@ -15,13 +18,18 @@ public class PlayerController : MonoBehaviour
     private BoxCollider _mainCollider;
     private Transform _cursor;
 
-    private Vector3 movementInput;
-
+    private Vector3 _movementInput;
+    private int _currentRecordCount;
+    private int _currentCatchFrames;
+    private GameObject _coyoteRecord;
+    
     private void Start()
     {
         _spriteRenderer = sprite.GetComponent<SpriteRenderer>();
         _mainCollider = GetComponent<BoxCollider>();
         _cursor = GameObject.FindWithTag("Cursor").transform;
+
+        _currentRecordCount = totalRecords;
 
         // Form collider for enemies and records and such
         Bounds spriteBounds = _spriteRenderer.bounds;
@@ -34,10 +42,10 @@ public class PlayerController : MonoBehaviour
         // Get input and move
         Vector2 movementInputNormalized =
             new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        movementInput = new Vector3(movementInputNormalized.x, 0, movementInputNormalized.y);
+        _movementInput = new Vector3(movementInputNormalized.x, 0, movementInputNormalized.y);
 
         // Record throwing
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && _currentRecordCount > 0)
         {
             Vector2 throwDir2d = (_cursor.position - Camera.main.WorldToScreenPoint(transform.position)).normalized;
             Vector3 throwDir = new Vector3(throwDir2d.x, 0f, throwDir2d.y);
@@ -46,11 +54,62 @@ public class PlayerController : MonoBehaviour
                 transform.position + throwDir * 1.5f,
                 Quaternion.identity);
             recordInstance.GetComponent<Rigidbody>().AddForce(throwDir * throwSpeed * 100);
+            _currentRecordCount--;
+        }
+
+        if (Input.GetButtonDown("Fire2") && _currentCatchFrames <= 0)
+        {
+            _currentCatchFrames = catchFramesTotal;
+        }
+        else if (Input.GetButtonDown("Fire2") && _coyoteRecord != null)
+        {
+            Destroy(_coyoteRecord);
+            _currentRecordCount++;
         }
     }
 
     private void FixedUpdate()
     {
-        transform.position += movementInput / 100 * walkSpeed;
+        transform.position += _movementInput / 100 * walkSpeed;
+
+        if (_currentCatchFrames > 0)
+        {
+            _currentCatchFrames--;
+        }
+        else
+        {
+            _currentCatchFrames = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Record"))
+        {
+            if (_currentCatchFrames > 0)
+            {
+                Destroy(other.gameObject);
+                _currentRecordCount++;
+            }
+            else
+            {
+                StartCoroutine(CollideWithRecord(other));
+            }
+        }
+    }
+
+    private IEnumerator CollideWithRecord(Collider other)
+    {
+        if (_currentCatchFrames > 0)
+        {
+            Destroy(other.gameObject);
+            _currentRecordCount++;
+        }
+        else
+        {
+            _coyoteRecord = other.gameObject;
+            yield return new WaitForSeconds(recordCoyoteSeconds);
+            _coyoteRecord = null;
+        }
     }
 }
