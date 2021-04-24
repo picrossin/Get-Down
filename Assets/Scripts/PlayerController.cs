cdbc30e66e22185c17cd5260115039881bbefc1a
picrossin <jadengoter@gmail.com>
2021-04-24 10:26:42 -0500

    Fixed level collisions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] [Range(0f, 50f)] private float speed;
+    [SerializeField] private GameObject sprite;
     
+    private SpriteRenderer _spriteRenderer;
+
+    private Vector3 movementInput;
+    
+    private void Start()
+    {
+        _spriteRenderer = sprite.GetComponent<SpriteRenderer>();
+    }
+
     private void Update()
     {
+        // Get input and move
         Vector2 movementInputNormalized =
             new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-        Vector3 movementInput = new Vector3(movementInputNormalized.x, 0, movementInputNormalized.y);
+        movementInput = new Vector3(movementInputNormalized.x, 0, movementInputNormalized.y);
+        
+        // Form collider for enemies and records and such
+    }
 
+    private void FixedUpdate()
+    {
         transform.position += movementInput / 100 * speed;
     }
 }
