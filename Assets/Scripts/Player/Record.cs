9d937f13b73e7cf2316e857894598497627925e7
picrossin <jadengoter@gmail.com>
2021-04-25 01:05:36 -0500

    Made record pickup stop

--- Assets/Scripts/Player/Record.cs
+++ Assets/Scripts/Player/Record.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class Record : MonoBehaviour
 {
+    [SerializeField] private int totalBounce = 4;
+    
     private int _bounces = 0;
     public int Bounces => _bounces;
+
+    private bool _onFloor;
+    public bool OnFloor => _onFloor;
 
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.layer == 3)
         {
             _bounces++;
+            if (_bounces >= totalBounce)
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+                GetComponent<SphereCollider>().isTrigger = true;
+                _onFloor = true;
+            }
         }
     }
 }
