fdf4b6ace8da283b343e48d166c49902bf98031a
picrossin <jadengoter@gmail.com>
2021-04-26 16:13:21 -0500

    Added quit button

---
+++ Assets/Scripts/Quitter.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Quitter : MonoBehaviour
+{
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
